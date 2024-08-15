da58e1396b4e0f2ed57314624b20ca9e9c1e8505
Ashwanipydev <pydev.ashwani@gmail.com>
2024-08-15 15:59:42 +0530

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -18,7 +18,10 @@ const router = createBrowserRouter(
       <Route path='user/:id' element={ <User />} />
       <Route loader={gitdatainfo} path='github' element={<Github />} />
     </Route>
-  )
+  ),
+  {
+    basename: "/React_Router_DOM", // Set basename to match your GitHub Pages subdirectory
+  }
 )
 
 createRoot(document.getElementById('root')).render(
