dc82cbdd901257421d2e927001e59654932374d2
Ashwanipydev <pydev.ashwani@gmail.com>
2024-08-15 15:35:22 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://ashwanipydev.github.io/React_Router_DOM/",
   plugins: [react()],
+  // "homepage": "https://ashwanipydev.github.io/React_Router_DOM/",
 })
